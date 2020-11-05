1e1e97a5189d42d761b7306cdfece1efb06172c6
Glenn Harper <glharper@microsoft.com>
2020-11-05 09:08:55 -0800

    Correctly send headers back as line-separated

--- server.js
+++ server.js
@@ -30,18 +30,75 @@ function loadScripts(path) {
 }
 
 loadScripts(scriptPath);
-console.log(scriptDict);
  
+let requestDict = {};
 server.on('connection', function connection(conn) 
 {
    conn.on('message', function incoming(message) 
    { 
       const headerBodySplit = message.toString().split("\r\n");
       if (headerBodySplit && headerBodySplit.length > 0) {
-         const headerFirstWord = headerBodySplit[0].split(" ")[0];
-         if(headerFirstWord === 'Path:') {
-            console.log('received: header %s', headerBodySplit[0]);
+         const requestId = headerBodySplit[1].split(" ")[1];
+         if(!(requestId in requestDict) && headerBodySplit[0] === 'Path: speech.config' && headerBodySplit[3] ==="Content-Type: application/json") {
+            let configDict = JSON.parse(headerBodySplit[5]);
+            if (configDict.context.system && 'lang' in configDict.context.system && 'testSuite' in configDict.context.system && 'testName' in configDict.context.system) {
+               let language = configDict.context.system['lang'];
+               if(language in scriptDict) {
+                  let testSuite = configDict.context.system['testSuite'];
+                  if(testSuite in scriptDict[language]) {
+                     let testName = configDict.context.system['testName'];
+                     if(testName in scriptDict[language][testSuite]) {
+                        requestDict[requestId] = { 
+                           'script': scriptDict[language][testSuite][testName],
+                           'index': 1,
+                           'testName': testName
+                        };
+                     }
+                  }
+               }
+            }
          } else {
+            if (requestId in requestDict) {
+
+               let entry = requestDict[requestId];
+               let currentIndex = entry['index'];
+               let messageType = entry['script'][currentIndex].split(" ")[0];
+
+               // First check if we expect a message from client
+               if (messageType === "Sent:") {
+                  currentIndex += 1;
+                  messageType = entry['script'][currentIndex].split(" ")[0];
+               }
+
+               // Send response(s) if script dictates
+               while (messageType === "Received:") {
+                  let responseMembers = entry['script'][currentIndex].split("|");
+                  let responseHeaderDict = JSON.parse(responseMembers[4].slice(responseMembers[4].indexOf("{")));
+                  let responseBody = JSON.parse(responseMembers[2].slice(responseMembers[2].indexOf("{")));
+                  responseHeaderDict["x-requestid"] = requestId;
+                  // let headers = responseMembers[4].slice(responseMembers[4].indexOf("{"), responseMembers[4].length - 1);
+                  let headers = "";
+                  for (var key in responseHeaderDict) {
+                     if (responseHeaderDict.hasOwnProperty(key)) {
+                        headers += key + ": " + responseHeaderDict[key] + "\r\n";
+                     }
+                  }
+                  let response = `${headers}\r\n${JSON.stringify(responseBody)}`;
+                  conn.send(response);
+                  currentIndex += 1;
+                  messageType = entry['script'][currentIndex].split(" ")[0];
+               }
+
+               // update index value in requestDict
+               requestDict[requestId]['index'] = currentIndex;
+
+               // if we've gotten to the end of the script, delete this entry in requestDict
+               if ( currentIndex >= entry['script'].length || !entry['script'][currentIndex] || entry['script'][currentIndex] === '') {
+                  delete requestDict[requestId];
+               }
+            }
+            /*
+            console.log('received: non-speech config message');
             const binaryMessage = Buffer.from(message)
             const headerLength = binaryMessage.readInt16BE(0);
 
@@ -49,7 +106,6 @@ server.on('connection', function connection(conn)
             for (let i = 0; i < headerLength; i++) {
                 headerString += String.fromCharCode(binaryMessage.readUInt8(i + 2));
             }
-            console.log('received: binary header length %s, content: %s', headerLength.toString(), headerString);
             let response = "Path: turn.end\r\n";
             if(binaryMessage.length > 2 + headerLength) {
                const pixelPosition = 0;
@@ -59,6 +115,7 @@ server.on('connection', function connection(conn)
                response = "Path: vision.color\r\n\r\n" + "color: " + color;
             }
             conn.send(response);
+            */
          
          }
       }
